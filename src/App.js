333e3271cd07e8413cc34549db0a3c8f294333df
vuthienson8 <thiensonvu08@gmail.com>
2022-04-06 10:26:50 +0700

    update codeBase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Suspense } from 'react';
 import './App.scss';
 import logo from './logo.svg';
-import Test from './pages/test';
 
 function App() {
   return (
     <Suspense fallback={<></>}>
-      <Test />
+      <div className='App'>
+        <header className='header'>
+          <img src={logo} className='logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className='link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+            Learn React
+          </a>
+        </header>
+      </div>
     </Suspense>
   );
 }
