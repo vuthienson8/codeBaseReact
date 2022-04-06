3e00704b24aaeb632a1bfa3ba9aeedd2ead611c1
vuthienson8 <thiensonvu08@gmail.com>
2022-04-06 10:37:09 +0700

    update code base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React, { Suspense } from 'react';
 import './App.scss';
-// import logo from './logo.svg';
-const Test = React.lazy(() => import('./pages/test'));
+import logo from './logo.svg';
 
 function App() {
   return (
-    <Suspense fallback={<></>}>
-      {/* <div className='App'>
-        <header className='header'>
-          <img src={logo} className='logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className='link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
-      </div> */}
-      <Test />
-    </Suspense>
+    <div className='App'>
+      <header className='header'>
+        <img src={logo} className='logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className='link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
